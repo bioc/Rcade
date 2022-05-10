e9c91204f5223bf21e63783314af3b530ee4617f
lshep <lori.shepherd@roswellpark.org>
2022-05-10 13:48:38 -0400

    Deprecated
     -  unresponsive

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,9 @@
 
 	packageStartupMessage("Welcome to Rcade - version ", packageDescription("Rcade", fields="Version"))
 	packageStartupMessage('If you are new to Rcade, please consider reading the vignette through the command: vignette("Rcade").')
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.17")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
 
